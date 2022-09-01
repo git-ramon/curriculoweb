a6551059a375161b5397b1d670ec436709a9d45b
ramon <ramonrodriguesnw@gmail.com>
2022-09-01 14:59:09 -0300

    funçaoes js para alterar cores de nav, span e links no click

--- js/scripts.js
+++ js/scripts.js
@@ -32,3 +32,70 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+/* Tema de Cores */
+
+const temas = document.querySelectorAll("#menu-color li");
+const cor = document.querySelector(".bg-primary");
+const cortext = document.querySelector(".text-primary");
+const corlink = document.querySelector(".linkmail");
+const text1 = document.querySelector(".text1");
+const text2 = document.querySelector(".text2");
+const text3 = document.querySelector(".text3");
+const text4 = document.querySelector(".text4");
+const text5 = document.querySelector(".text5");
+const text6 = document.querySelector(".text6");
+const iconlinkedin = document.querySelector(".link-icon-linkedin");
+const icongithub = document.querySelector(".link-icon-github");
+const iconinsta = document.querySelector(".link-icon-insta");
+const iconwhatsapp = document.querySelector(".link-icon-whatsapp");
+
+
+temas.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+
+    temas.forEach((btn) =>
+      btn.querySelector(".color").classList.remove("selected")
+    );
+
+    const button = e.target;
+
+    const id = button.getAttribute("id");
+
+    /*button.querySelector(".color").classList.add("selected");*/
+
+    cor.setAttribute("style", `background-color: ${id} !important`);
+    cortext.setAttribute("style", `color: ${id} !important`);
+    corlink.setAttribute("style", `color: ${id} !important`);
+    text1.setAttribute("style", `color: ${id} !important`);
+    text2.setAttribute("style", `color: ${id} !important`);
+    text3.setAttribute("style", `color: ${id} !important`);
+    text4.setAttribute("style", `color: ${id} !important`);
+    text5.setAttribute("style", `color: ${id} !important`);
+    text6.setAttribute("style", `color: ${id} !important`);
+    iconlinkedin.setAttribute("onmouseover", `this.style.backgroundColor='${id}';`);
+    iconlinkedin.setAttribute("onmouseout", `this.style.backgroundColor='#001219';`);
+    icongithub.setAttribute("onmouseover", `this.style.backgroundColor='${id}';`);
+    icongithub.setAttribute("onmouseout", `this.style.backgroundColor='#001219';`);
+    iconinsta.setAttribute("onmouseover", `this.style.backgroundColor='${id}';`);
+    iconinsta.setAttribute("onmouseout", `this.style.backgroundColor='#001219';`);
+    iconwhatsapp.setAttribute("onmouseover", `this.style.backgroundColor='${id}';`);
+    iconwhatsapp.setAttribute("onmouseout", `this.style.backgroundColor='#001219';`);
+    
+  });
+});
+
+function setcolorStorage() {
+    localStorage.setItem('cor', 1);
+  }
+
+  var span = document.getElementById("span");
+
+  span.addEventListener("click", setcolorStorage);
+
+/*********************** */
+
+
+
+
