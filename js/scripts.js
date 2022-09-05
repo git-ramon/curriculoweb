583c66f75144b9bb0775b736f1ba1ead1f292112
ramon <ramonrodriguesnw@gmail.com>
2022-09-04 21:37:14 -0300

    cor tema aplicado em links, icns e textos e salvo em local storage, e refatoramento de codigo.

--- js/scripts.js
+++ js/scripts.js
@@ -22,12 +22,9 @@ window.addEventListener('DOMContentLoaded', event => {
       }
     });
   });
-
 });
 
-
 /* Tema de Cores */
-
 const temas = document.querySelectorAll("#menu-color li");
 const cor = document.querySelector(".bg-primary");
 const cortext = document.querySelector(".text-primary");
@@ -43,139 +40,62 @@ const icongithub = document.querySelector(".link-icon-github");
 const iconinsta = document.querySelector(".link-icon-insta");
 const iconwhatsapp = document.querySelector(".link-icon-whatsapp");
 
+/* Salvando cor do tema em localstorage */
 const criastorage = () => {
-    document.querySelectorAll("#menu-color li").forEach( function(button) {
-    
-    button.addEventListener("click", function(event) {
-    const el = event.target;
-    const id = el.id;
+  document.querySelectorAll("#menu-color li").forEach(function (button) {
 
-    window.localStorage.setItem('cor', id)
+    button.addEventListener("click", function (event) {
+      const el = event.target;
+      const id = el.id;
 
-  });  
-});
+      window.localStorage.setItem('cor', id)
 
+      location.reload();
+    });
+  });
 };
 
 criastorage();
 
+/* Pegando cor do tema em localstorage */
 const getstorage = () => {
   const getcor = window.localStorage.getItem('cor');
 
   if (getcor) {
     cor.classList.remove('bg-primary');
     cor.classList.add(getcor)
-  }
-  
-    temas.forEach((btn) => {
-
-      btn.addEventListener("click", (e) => { 
-
-
-    const button = e.target;
-
-    const id = button.getAttribute("id");
-
-        cor.setAttribute("style", `background-color: ${id} !important`);
-        cortext.setAttribute("style", `color: ${id} !important`);
-        corlink.setAttribute("style", `color: ${id} !important`);
-        text1.setAttribute("style", `color: ${id} !important`);
-        text2.setAttribute("style", `color: ${id} !important`);
-        text3.setAttribute("style", `color: ${id} !important`);
-        text4.setAttribute("style", `color: ${id} !important`);
-        text5.setAttribute("style", `color: ${id} !important`);
-        text6.setAttribute("style", `color: ${id} !important`);
-        iconlinkedin.setAttribute("onmouseover", `this.style.backgroundColor='${id}';`);
-        iconlinkedin.setAttribute("onmouseout", `this.style.backgroundColor='#001219';`);
-        icongithub.setAttribute("onmouseover", `this.style.backgroundColor='${id}';`);
-        icongithub.setAttribute("onmouseout", `this.style.backgroundColor='#001219';`);
-        iconinsta.setAttribute("onmouseover", `this.style.backgroundColor='${id}';`);
-        iconinsta.setAttribute("onmouseout", `this.style.backgroundColor='#001219';`);
-        iconwhatsapp.setAttribute("onmouseover", `this.style.backgroundColor='${id}';`);
-        iconwhatsapp.setAttribute("onmouseout", `this.style.backgroundColor='#001219';`);
-
-        location.reload();
-      });
-    });    
-};
-
-getstorage();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* Metodo 02 */
-/*
-let pink = document.getElementById('sideNav');
-const cortext = document.querySelector('.text-primary');
-const corlinkmail = document.querySelector('.linkmail');
-const cortext1 = document.querySelector('.text1');
-const cortext2 = document.querySelector('.text2');
-const cortext3 = document.querySelector('.text3');
-const cortext4 = document.querySelector('.text4');
-const cortext5 = document.querySelector('.text5');
-const cortext6 = document.querySelector('.text6');
-const temas = document.querySelectorAll("#menu-color li");
-let select = document.querySelector('.rosa');
-
-const criastorage = () => {
-  window.localStorage.setItem('cor', 'rosa')
-
-  location.reload();
-};
-
-const getstorage = () => {
-  const cor = window.localStorage.getItem('cor');
-
-  if (cor) {
-    pink.classList.remove('bg-primary');
-    pink.classList.add('rosa');
 
     cortext.classList.remove('text-primary');
-    cortext.classList.add('colortextrosa')
-    
-    corlinkmail.classList.remove('text-primary');
-    corlinkmail.classList.add('colortextrosa')
+    corlink.classList.remove('linkmail');
+    text1.classList.remove('text-primary');
+    text2.classList.remove('text-primary');
+    text3.classList.remove('text-primary');
+    text4.classList.remove('text-primary');
+    text5.classList.remove('text-primary');
+    text6.classList.remove('text-primary');
 
-    cortext1.classList.remove('text1', 'text-primary');
-    cortext1.classList.add('colortextrosa')
-    cortext2.classList.remove('text2', 'text-primary');
-    cortext2.classList.add('colortextrosa')
-    cortext3.classList.remove('text3', 'text-primary');
-    cortext3.classList.add('colortextrosa')
-    cortext4.classList.remove('text4', 'text-primary');
-    cortext4.classList.add('colortextrosa')
-    cortext5.classList.remove('text5', 'text-primary');
-    cortext5.classList.add('colortextrosa')
-    cortext6.classList.remove('text6', 'text-primary');
-    cortext6.classList.add('colortextrosa')
+    cortext.setAttribute("style", `color: var(--${getcor})`);
+    corlink.setAttribute("style", `color: var(--${getcor})`);
+    text1.setAttribute("style", `color: var(--${getcor})`);
+    text2.setAttribute("style", `color: var(--${getcor})`);
+    text3.setAttribute("style", `color: var(--${getcor})`);
+    text4.setAttribute("style", `color: var(--${getcor})`);
+    text5.setAttribute("style", `color: var(--${getcor})`);
+    text6.setAttribute("style", `color: var(--${getcor})`);
 
-    temas.forEach((btn) =>
-      btn.querySelector(".color").classList.remove("selected")
-    );
-    
-    select.classList.add('selected');
+    iconlinkedin.setAttribute("style", `background-color: var(--${getcor}) !important`);
+    iconlinkedin.setAttribute("onmouseover", `this.style.backgroundColor='#001219';`);
+    iconlinkedin.setAttribute("onmouseout", `this.style.backgroundColor='var(--${getcor})';`);
+    icongithub.setAttribute("style", `background-color: var(--${getcor}) !important`);
+    icongithub.setAttribute("onmouseover", `this.style.backgroundColor='#001219';`);
+    icongithub.setAttribute("onmouseout", `this.style.backgroundColor='var(--${getcor})';`);
+    iconinsta.setAttribute("style", `background-color: var(--${getcor}) !important`);
+    iconinsta.setAttribute("onmouseover", `this.style.backgroundColor='#001219';`);
+    iconinsta.setAttribute("onmouseout", `this.style.backgroundColor='var(--${getcor})';`);
+    iconwhatsapp.setAttribute("style", `background-color: var(--${getcor}) !important`);
+    iconwhatsapp.setAttribute("onmouseover", `this.style.backgroundColor='#001219';`);
+    iconwhatsapp.setAttribute("onmouseout", `this.style.backgroundColor='var(--${getcor})';`);
   }
 };
 
-document.getElementById('pink').onclick = criastorage;
-
 getstorage();
-
-*/
